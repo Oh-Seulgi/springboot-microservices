1f77e969cb5113928109b84f846ed3a53b8d0558
Oh-Seulgi <tmfrl5584@naver.com>
2024-06-09 18:22:58 +0900

    1. boardservice : WebMvcConfig 추가 (프론트 협업을 위한 임시)

---
+++ board-service/src/main/java/github/seulgi/boardservice/global/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package github.seulgi.boardservice.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // CORS를 적용할 엔드포인트 패턴 지정
+                .allowedOrigins("http://localhost:3000") // 허용할 출처 지정
+                .allowedMethods("GET", "POST", "PUT", "DELETE"); // 허용할 HTTP 메서드 지정
+    }
+}
